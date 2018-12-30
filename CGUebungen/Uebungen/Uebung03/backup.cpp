072449c70e334e754a5f884759d5f22d81875bfd
simon-winter <you@example.com>
2018-12-30 16:52:55 +0100

    fix attempt 1 pro edit

--- CGUebungen/Uebungen/Uebung03/backup.cpp
+++ CGUebungen/Uebungen/Uebung03/backup.cpp
@@ -1,3 +1,47 @@
+//#-------------------------------------------------
+//#
+//# Project created by QtCreator 2011-03-04T17:34:20
+//#
+//#-------------------------------------------------
+
+//include(../../Common.pri)
+//TARGET = Uebung03
+//SOURCES += \
+//    kopf.cpp \
+//    bodypart.cpp \
+//    arm.cpp \
+//    myscene.cpp \
+//    world.cpp \
+//    body.cpp \
+//    pipe.cpp \
+//    tower.cpp \
+//    gameloop.cpp \
+//    cannonball.cpp \
+//    backup.cpp \
+//    projectile.cpp
+
+//FORMS += \
+//    robot.ui
+
+//HEADERS += \
+//    world.h \
+//    body.h \
+//    pipe.h \
+//    tower.h \
+//    gameloop.h \
+//<<<<<<< HEAD
+//    cannonball.h
+
+//RESOURCES += \
+//    MyShaders.qrc
+//=======
+//    cannonball.h \
+//    projectile.h
+//>>>>>>> 7e2d0e4efffefc004273a91c6d8cb4c08f03503a
+
+
+
+
 //#include "opengl.h"
 //#include "scenemanager.h"
 //#include "transformation.h"
